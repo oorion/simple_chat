2d40d21a868c412fe2d0088f780327d298667cfa
Josh Cheek <josh.cheek@gmail.com>
2015-10-25 16:38:31 -0600

    Fix the integration test

--- spec/integration_test.spec.js
+++ spec/integration_test.spec.js
@@ -6,21 +6,41 @@ var options ={
   'force new connection': true
 };
 
-describe("Chat Server",function(done){
-  it('should return the same randomString to two users if both send waiting', function(){
+describe("Chat Server",function(){
+  it('should return the same randomString to two users if both send waiting', function(done){
     var client1 = io.connect(socketURL, options);
+    var zipCode = '12345';
+
+    // var oldEmit  = client1.emit;
+    // client1.emit = function (type) {
+    //   return oldEmit.apply(this, arguments);
+    // }
+
     client1.on('connect', function(data){
-      client1.emit('waiting', ['a1', '12345']);
+      client1.emit('waiting', ['a1', zipCode]);
       var client2 = io.connect(socketURL, options);
 
-      client1.on('connect', function(data){
-        console.log('here');
-        client2.emit('waiting', ['a2', '12345']);
+      client2.on('connect', function(data){
+        client2.emit('waiting', ['a2', zipCode]);
+
+        var dataSeen = [];
+
+        function checkData(index, data) {
+          dataSeen[index] = data;
+          if(dataSeen[0] && dataSeen[1]) {
+            expect(dataSeen[0]).toEqual(dataSeen[1]);
+            client1.disconnect();
+            client2.disconnect();
+            done();
+          }
+        }
+
+        client1.on('new-connection', function(data){
+          checkData(0, data);
+        });
 
         client2.on('new-connection', function(data){
-          console.log('client2 data: ' + data);
-          client2.disconnect();
-          done();
+          checkData(1, data);
         });
       });
     });
